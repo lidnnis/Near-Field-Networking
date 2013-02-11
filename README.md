a3ed91db20a5c090121dbeac518adaf9d93e5d27
lidnnis <lidnnis@gmail.com>
2013-02-10 21:05:10 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Near-Field-Networking
+<<<<<<< HEAD
+=====================
+
+Please use the dev branch for development
+-----------------------------------------
+
+
+Created by
+- Dennis
+- Faizan
+- Jamie
+- Calvin
+- Vince
+
+=======
+=====================
+>>>>>>> 55fcfa2... Initial commit
---
+++ src/com/example/nearfieldnetworking/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.nearfieldnetworking;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	        case R.id.menu_nfc:
+	            launchNFCActivity();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	
+	public void launchNFCActivity()
+	{
+		   Intent intent = new Intent(this, NFCActivity.class);
+		   startActivity(intent);
+	}
+
+}

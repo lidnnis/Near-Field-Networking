e1f66e7b21d49b5f9cdb662680428355593e42b8
vC- <vincentwcheung@gmail.com>
2013-02-25 20:00:07 -0800

    Back Button.. again

--- src/com/example/nearfieldnetworking/EditPersonActivity.java
+++ src/com/example/nearfieldnetworking/EditPersonActivity.java
@@ -35,6 +35,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.app.ActionBar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /*************************************************************************
 *	EditPersonActivity
